e970e4a119c5e7d3df131b9e8629f2792c7ea885
n4j1Br4ch1D <you@example.com>
2022-11-12 11:38:57 +0100

    disable signup btn

--- src/sections/Navbar.tsx
+++ src/sections/Navbar.tsx
@@ -47,7 +47,7 @@ function Navbar() {
                             </ul>
                         </div>
                     </div>
-                    <button className="focus:outline-none lg:text-lg lg:font-bold focus:ring-2 focus:ring-offset-2 focus:ring-indigo-700 hidden md:block bg-transparent transition duration-150 ease-in-out hover:bg-gray-200 rounded border border-indigo-700 text-indigo-700 px-4 sm:px-8 py-1 sm:py-3 text-sm">Sign In</button>
+                    <button disabled className="focus:outline-none lg:text-lg lg:font-bold focus:ring-2 focus:ring-offset-2 focus:ring-indigo-700 hidden md:block bg-transparent transition duration-150 ease-in-out hover:bg-gray-200 rounded border border-indigo-700 text-indigo-700 px-4 sm:px-8 py-1 sm:py-3 text-sm">Sign In</button>
                 </div>
             </nav>
 
