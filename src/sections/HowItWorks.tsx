c4e1f952120d9c971d133229407858fa1c82007a
n4j1Br4ch1D <you@example.com>
2022-11-14 13:02:40 +0100

    fix some linting warnings

--- src/sections/HowItWorks.tsx
+++ src/sections/HowItWorks.tsx
@@ -1,92 +1,141 @@
 function HowItWorks() {
   return (
-
-
-<section className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
-  <div className="max-w-xl mb-10 md:mx-auto sm:text-center lg:max-w-2xl md:mb-12">
-    <h2 className="font-bold text-indigo-900 text-4xl text-center ">
-     Easy-peasy, Check How it works.
-    </h2>
-  </div>
-  <div className="grid gap-8 row-gap-0 lg:grid-cols-3">
-    <div className="relative text-center">
-
-
-
-      <div className="relative flex items-center text-3xl font-black justify-center w-16 h-16 mx-auto mb-4 rounded-full bg-indigo-50 sm:w-20 sm:h-20">
-        1
-
-         <div className="inline-flex absolute -top-4 -right-4 justify-center items-center w-12 h-12 text-xs font-bold text-white bg-purple-700 rounded-full border-2 border-white dark:border-gray-900">
-<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5">
-  <path d="M2 4.25A2.25 2.25 0 014.25 2h2.5A2.25 2.25 0 019 4.25v2.5A2.25 2.25 0 016.75 9h-2.5A2.25 2.25 0 012 6.75v-2.5zM2 13.25A2.25 2.25 0 014.25 11h2.5A2.25 2.25 0 019 13.25v2.5A2.25 2.25 0 016.75 18h-2.5A2.25 2.25 0 012 15.75v-2.5zM11 4.25A2.25 2.25 0 0113.25 2h2.5A2.25 2.25 0 0118 4.25v2.5A2.25 2.25 0 0115.75 9h-2.5A2.25 2.25 0 0111 6.75v-2.5zM15.25 11.75a.75.75 0 00-1.5 0v2h-2a.75.75 0 000 1.5h2v2a.75.75 0 001.5 0v-2h2a.75.75 0 000-1.5h-2v-2z"></path>
-</svg>
-        </div>
-
+    <section
+      id="how-it-works"
+      className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20"
+    >
+      <div className="max-w-xl mb-10 md:mx-auto sm:text-center lg:max-w-2xl md:mb-12">
+        <h2 className="font-bold text-indigo-900 text-4xl text-center ">
+          Easy-peasy, Check How it works.
+        </h2>
       </div>
-      <div className="flex flex-col items-center justify-center">
-        <div className="w-52 text-center">
-          <p className="max-w-md mb-3 text-sm text-gray-900 sm:mx-auto">
-            Pick from our <span className="font-bold">Already Built Templates </span> 
-            or Create
-             <span className="font-bold">New Template</span>
-          </p>
+      <div className="grid gap-8 row-gap-0 lg:grid-cols-3">
+        <div className="relative text-center">
+          <div className="relative flex items-center text-3xl font-black justify-center w-16 h-16 mx-auto mb-4 rounded-full bg-indigo-50 sm:w-20 sm:h-20">
+            1
+            <div className="inline-flex absolute -top-4 -right-4 justify-center items-center w-12 h-12 text-xs font-bold text-white bg-purple-700 rounded-full border-2 border-white dark:border-gray-900">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+                className="w-5 h-5"
+              >
+                <path d="M2 4.25A2.25 2.25 0 014.25 2h2.5A2.25 2.25 0 019 4.25v2.5A2.25 2.25 0 016.75 9h-2.5A2.25 2.25 0 012 6.75v-2.5zM2 13.25A2.25 2.25 0 014.25 11h2.5A2.25 2.25 0 019 13.25v2.5A2.25 2.25 0 016.75 18h-2.5A2.25 2.25 0 012 15.75v-2.5zM11 4.25A2.25 2.25 0 0113.25 2h2.5A2.25 2.25 0 0118 4.25v2.5A2.25 2.25 0 0115.75 9h-2.5A2.25 2.25 0 0111 6.75v-2.5zM15.25 11.75a.75.75 0 00-1.5 0v2h-2a.75.75 0 000 1.5h2v2a.75.75 0 001.5 0v-2h2a.75.75 0 000-1.5h-2v-2z"></path>
+              </svg>
+            </div>
+          </div>
+          <div className="flex flex-col items-center justify-center">
+            <div className="w-52 text-center">
+              <p className="max-w-md mb-3 text-sm text-gray-900 sm:mx-auto">
+                Pick from our{' '}
+                <span className="font-bold">Already Built Templates </span>
+                or Create
+                <span className="font-bold">New Template</span>
+              </p>
+            </div>
+          </div>
+          <div className="top-0 right-0 flex items-center justify-center h-24 lg:-mr-8 lg:absolute">
+            <svg
+              className="w-8 text-gray-700 transform rotate-90 lg:rotate-0"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              viewBox="0 0 24 24"
+            >
+              <line
+                fill="none"
+                stroke-miterlimit="10"
+                x1="2"
+                y1="12"
+                x2="22"
+                y2="12"
+              ></line>
+              <polyline
+                fill="none"
+                stroke-miterlimit="10"
+                points="15,5 22,12 15,19 "
+              ></polyline>
+            </svg>
+          </div>
+        </div>
+        <div className="relative text-center">
+          <div className="relative flex items-center text-3xl font-black justify-center w-16 h-16 mx-auto mb-4 rounded-full bg-indigo-50 sm:w-20 sm:h-20">
+            2
+            <div className="inline-flex absolute -top-4 -right-4 justify-center items-center w-12 h-12 text-xs font-bold text-white bg-purple-700 rounded-full border-2 border-white dark:border-gray-900">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+                className="w-5 h-5"
+              >
+                <path d="M5.433 13.917l1.262-3.155A4 4 0 017.58 9.42l6.92-6.918a2.121 2.121 0 013 3l-6.92 6.918c-.383.383-.84.685-1.343.886l-3.154 1.262a.5.5 0 01-.65-.65z"></path>
+                <path d="M3.5 5.75c0-.69.56-1.25 1.25-1.25H10A.75.75 0 0010 3H4.75A2.75 2.75 0 002 5.75v9.5A2.75 2.75 0 004.75 18h9.5A2.75 2.75 0 0017 15.25V10a.75.75 0 00-1.5 0v5.25c0 .69-.56 1.25-1.25 1.25h-9.5c-.69 0-1.25-.56-1.25-1.25v-9.5z"></path>
+              </svg>
+            </div>
+          </div>
+          <div className="flex flex-col items-center justify-center">
+            <div className="w-52 text-center">
+              <p className="max-w-md mb-3 text-sm text-gray-900 sm:mx-auto">
+                Search, Filter by{' '}
+                <span className="font-bold">technologies & stacks</span>, pick ,
+                and start editing your{' '}
+                <span className="font-bold">New Template</span>
+              </p>
+            </div>
+            <div className="top-0 right-0 flex items-center justify-center h-24 lg:-mr-8 lg:absolute">
+              <svg
+                className="w-8 text-gray-700 transform rotate-90 lg:rotate-0"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                viewBox="0 0 24 24"
+              >
+                <line
+                  fill="none"
+                  stroke-miterlimit="10"
+                  x1="2"
+                  y1="12"
+                  x2="22"
+                  y2="12"
+                ></line>
+                <polyline
+                  fill="none"
+                  stroke-miterlimit="10"
+                  points="15,5 22,12 15,19 "
+                ></polyline>
+              </svg>
+            </div>
+          </div>
+        </div>
+        <div className="relative text-center">
+          <div className="relative flex items-center text-3xl font-black justify-center w-16 h-16 mx-auto mb-4 rounded-full bg-indigo-50 sm:w-20 sm:h-20">
+            3
+            <div className="inline-flex absolute -top-4 -right-4 justify-center items-center w-12 h-12 text-xs font-bold text-white bg-purple-700 rounded-full border-2 border-white dark:border-gray-900">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+                className="w-5 h-5"
+              >
+                <path d="M10.75 2.75a.75.75 0 00-1.5 0v8.614L6.295 8.235a.75.75 0 10-1.09 1.03l4.25 4.5a.75.75 0 001.09 0l4.25-4.5a.75.75 0 00-1.09-1.03l-2.955 3.129V2.75z"></path>
+                <path d="M3.5 12.75a.75.75 0 00-1.5 0v2.5A2.75 2.75 0 004.75 18h10.5A2.75 2.75 0 0018 15.25v-2.5a.75.75 0 00-1.5 0v2.5c0 .69-.56 1.25-1.25 1.25H4.75c-.69 0-1.25-.56-1.25-1.25v-2.5z"></path>
+              </svg>
+            </div>
+          </div>
+          <div className="flex flex-col items-center justify-center">
+            <div className="w-52 text-center">
+              <p className="max-w-md mb-3 text-sm text-gray-900 sm:mx-auto">
+                <span className="font-bold">That's it. you'he all set!</span>{' '}
+                Save &amp; Download Your Template
+                <span className="block text-4xl">🎉</span>
+              </p>
+            </div>
+          </div>
         </div>
       </div>
-      <div className="top-0 right-0 flex items-center justify-center h-24 lg:-mr-8 lg:absolute">
-        <svg className="w-8 text-gray-700 transform rotate-90 lg:rotate-0" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" viewBox="0 0 24 24">
-          <line fill="none" stroke-miterlimit="10" x1="2" y1="12" x2="22" y2="12"></line>
-          <polyline fill="none" stroke-miterlimit="10" points="15,5 22,12 15,19 "></polyline>
-        </svg>
-      </div>
-    </div>
-    <div className="relative text-center">
-      <div className="relative flex items-center text-3xl font-black justify-center w-16 h-16 mx-auto mb-4 rounded-full bg-indigo-50 sm:w-20 sm:h-20">
-        2
-
-         <div className="inline-flex absolute -top-4 -right-4 justify-center items-center w-12 h-12 text-xs font-bold text-white bg-purple-700 rounded-full border-2 border-white dark:border-gray-900">
-<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5">
-  <path d="M5.433 13.917l1.262-3.155A4 4 0 017.58 9.42l6.92-6.918a2.121 2.121 0 013 3l-6.92 6.918c-.383.383-.84.685-1.343.886l-3.154 1.262a.5.5 0 01-.65-.65z"></path>
-  <path d="M3.5 5.75c0-.69.56-1.25 1.25-1.25H10A.75.75 0 0010 3H4.75A2.75 2.75 0 002 5.75v9.5A2.75 2.75 0 004.75 18h9.5A2.75 2.75 0 0017 15.25V10a.75.75 0 00-1.5 0v5.25c0 .69-.56 1.25-1.25 1.25h-9.5c-.69 0-1.25-.56-1.25-1.25v-9.5z"></path>
-</svg>
-        </div>
-      </div>
-      <div className="flex flex-col items-center justify-center">
-        <div className="w-52 text-center">
-          <p className="max-w-md mb-3 text-sm text-gray-900 sm:mx-auto">
-           Search, Filter by <span className="font-bold">technologies & stacks</span>, pick , and start editing your <span className="font-bold">New Template</span>
-          </p>
-        </div>
-        <div className="top-0 right-0 flex items-center justify-center h-24 lg:-mr-8 lg:absolute">
-          <svg className="w-8 text-gray-700 transform rotate-90 lg:rotate-0" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" viewBox="0 0 24 24">
-            <line fill="none" stroke-miterlimit="10" x1="2" y1="12" x2="22" y2="12"></line>
-            <polyline fill="none" stroke-miterlimit="10" points="15,5 22,12 15,19 "></polyline>
-          </svg>
-        </div>
-      </div>
-    </div>
-    <div className="relative text-center">
-      <div className="relative flex items-center text-3xl font-black justify-center w-16 h-16 mx-auto mb-4 rounded-full bg-indigo-50 sm:w-20 sm:h-20">
-        3
-
-         <div className="inline-flex absolute -top-4 -right-4 justify-center items-center w-12 h-12 text-xs font-bold text-white bg-purple-700 rounded-full border-2 border-white dark:border-gray-900">
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5">
-  <path d="M10.75 2.75a.75.75 0 00-1.5 0v8.614L6.295 8.235a.75.75 0 10-1.09 1.03l4.25 4.5a.75.75 0 001.09 0l4.25-4.5a.75.75 0 00-1.09-1.03l-2.955 3.129V2.75z"></path>
-  <path d="M3.5 12.75a.75.75 0 00-1.5 0v2.5A2.75 2.75 0 004.75 18h10.5A2.75 2.75 0 0018 15.25v-2.5a.75.75 0 00-1.5 0v2.5c0 .69-.56 1.25-1.25 1.25H4.75c-.69 0-1.25-.56-1.25-1.25v-2.5z"></path>
-</svg>
-
-        </div>
-      </div>
-      <div className="flex flex-col items-center justify-center">
-        <div className="w-52 text-center">
-          <p className="max-w-md mb-3 text-sm text-gray-900 sm:mx-auto">
-            <span className="font-bold">That's it. you'he all set!</span> Save &amp; Download Your Template
-            <span className="block text-4xl">🎉</span>
-          </p>
-        </div>
-      </div>
-    </div>
-  </div></section>
-
+    </section>
   );
 }
 
