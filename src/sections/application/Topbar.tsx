5381d80f8662b1c29c8425dc5226a8ba873f43ff
n4j1Br4ch1D <you@example.com>
2022-11-14 11:51:02 +0100

    suggest exitfullscreen icon

--- src/sections/application/Topbar.tsx
+++ src/sections/application/Topbar.tsx
@@ -1,5 +1,6 @@
 function Topbar() {
 
+  // converte to state later;
   let isFullscreen: boolean = false;
 
   function toggleFullscreen(toggle: boolean) {
@@ -65,11 +66,12 @@ function Topbar() {
               </svg>
               <span className="sr-only">Icon description</span>
             </button>
-
-
             <button onClick={() => toggleFullscreen(isFullscreen)} type="button" className="text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:outline-none focus:ring-purple-300 font-medium rounded-lg text-sm p-2.5 text-center inline-flex items-center dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-800">
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 3.75v4.5m0-4.5h4.5m-4.5 0L9 9M3.75 20.25v-4.5m0 4.5h4.5m-4.5 0L9 15M20.25 3.75h-4.5m4.5 0v4.5m0-4.5L15 9m5.25 11.25h-4.5m4.5 0v-4.5m0 4.5L15 15" />
+              </svg>
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="hidden w-6 h-6">
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M9 9V4.5M9 9H4.5M9 9L3.75 3.75M9 15v4.5M9 15H4.5M9 15l-5.25 5.25M15 9h4.5M15 9V4.5M15 9l5.25-5.25M15 15h4.5M15 15v4.5m0-4.5l5.25 5.25" />
               </svg>
               <span className="sr-only">Icon description</span>
             </button>
