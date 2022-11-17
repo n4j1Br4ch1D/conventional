fa14ffc784b7d5cb1d188265b3d8d2271844f0c3
n4j1Br4ch1D <you@example.com>
2022-11-17 17:41:01 +0100

    add privacy toggler to TemplateForm

--- src/sections/application/TemplateForm.tsx
+++ src/sections/application/TemplateForm.tsx
@@ -72,6 +72,21 @@ function TemplateForm() {
           ></textarea>
         </div>
         <div className="mb-6">
+          <label
+            htmlFor="message"
+            className="block mb-2 text-lg font-medium text-gray-900 dark:text-gray-400"
+          >
+            Set Privacy
+          </label>
+          <label className="inline-flex relative items-center mr-5 cursor-pointer">
+            <input type="checkbox" value="" className="sr-only peer" />
+            <div className="w-11 h-6 bg-gray-200 rounded-full peer dark:bg-gray-700 peer-focus:ring-4 peer-focus:ring-purple-300 dark:peer-focus:ring-purple-800 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-purple-600"></div>
+            <span className="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300">
+              Make Private
+            </span>
+          </label>
+        </div>
+        <div className="mb-6">
           <button
             type="button"
             className="text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:outline-none focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-800"
